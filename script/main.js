c78f5c932627e4467e137f2bd4f005e2719ee78b
Bruno Silva <https://github.com/Brn677>
2023-11-13 21:49:11 -0300

    inclusão da barra de pesquisa para os cânticos

---
+++ script/main.js
@@ -0,0 +1,15 @@
+document.getElementById("searchBar").addEventListener("input", function () {   //Barra de pesquisa
+
+  const termo = this.value.toLowerCase();
+  const tabela = document.getElementById("tabela");
+  const linhas = tabela.getElementsByTagName("tr");
+
+  for (let i = 1; i < linhas.length; i++) {
+    let texto = linhas[i].textContent.trim().charAt(0).toLowerCase();
+    if (texto.includes(termo)) {
+      linhas[i].style.display = "";
+    } else {
+      linhas[i].style.display = "none";
+    }
+  }
+});
